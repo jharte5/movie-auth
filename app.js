eb26fd02c433878d93859bd069badc35fb843636
jharte5 <jonathan.harte@codeimmersives.com>
2020-03-02 11:06:37 -0500

    so far

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const session  = require('express-session');
+let MongoStore = require('connect-mongo')(session);
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users/userRoutes');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(() => {
+  console.log('Mongodb connected');
+})
+.catch(err => console.log(`Mongodb error: ${err}`));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +34,47 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// create session middleware
+app.use(session({
+  resave:true,
+  saveUninitialized: true,
+  // required
+  secret: process.env.SESSION_SECRET,
+  // store info in datatbase
+  store: new MongoStore({
+    // url: process.env.MONGODB_URI,
+    mongooseConnection: mongoose.connection,
+    autoReconnect: true,
+  }),
+  cookie: {
+    secure: false,
+    maxAge: 60000
+  }
+})
+);
+
+// initialize passport module
+app.use(passport.initialize());
+
+// create session THIS MUST BE BELOW SESSION MIDDLE WARE
+app.use(passport.session());
+
+// locals allow us to create variables to use throughout
+app.use(
+  (req, res, next) => {
+    // passport gives us the use of req.user which we define as user
+    res.locals.user = req.user;
+    next();
+    
+  }
+);
+
+//router middleware 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
+  
+  // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const User = require('./models/Users');
+const { check, validationResult } = require('express-validator');
 
 /* find all users */
 router.get('/', function(req, res, next) {
-  // empty object allows us to fill with users
-  User.find({}).then(users => {
-    return res.status(200).json({message: 'Success', users})
-  })
-  .catch(err=> res.status(500).json({message:'Server Error', err}))
+  // // empty object allows us to fill with users
+  // User.find({}).then(users => {
+  //   return res.status(200).json({message: 'Success', users})
+  // })
+  // .catch(err=> res.status(500).json({message:'Server Error', err}))
+  return res.render('index')
 });
 
 router.get('/success', (req, res) => {
@@ -23,5 +26,108 @@ router.get('/success', (req, res) => {
 router.get('/fail', (req, res) => {
   return res.render('fail')
 });
+
+const myValidation = (req, res, next) => {
+  if (!req.body.name || !req.body.email || !req.body.password) {
+    return res.status(403).json({ message: 'All Inputs Must Be Filled' });
+  }
+  next();
+};
+
+router.post('/register', myValidation, (req, res) => {
+  // validate the inputs
+  
+  // check if user exists
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      //check to see if there is a user value
+      if (user) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+      // create a new user from the User model
+      const newUser = new User();
+
+      // salt password...place extra characters in password to make harder to guess
+      const salt = bcrypt.genSaltSync(10);
+      // hash password
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      // set values for the user to model keys
+      newUser.name = req.body.name;
+      newUser.email = req.body.email;
+      newUser.password = hash;
+      // save the user
+      newUser
+        .save()
+        .then(user => {
+          return req.login(user, (err) => {
+            if(err){
+              return res.status(500).json({message: 'Server Error', err});
+            } else {
+              console.log(req.session)
+              res.redirect('/users/success');
+            }
+          })
+        })
+        .catch(err => res.status(400).json({ message: 'User not saved', err }));
+    })
+    .catch(err => res.status(418).json({ message: 'We messed up', err }));
+});
+
+router.post('/login', 
+  // authenticate using local login from passport file
+  passport.authenticate('local-login', {
+    successRedirect:'/users/success',
+    failureRedirect: '/users/fail',
+    failureFlash: true
+  })
+);
+
+router.post('/api/users/register', 
+  [check('name', 'Name is required').not().isEmpty(), 
+  check('email', 'Please include a valid email').isEmail(), 
+  check('password', 'Please include valid password').isLength({min: 3})], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        console.log(errors);
+        return res.render('register', { errors: 'All inputs must be filled'});
+    };
+    
+    User.findOne({ email:req.body.email})
+    .then((user) => {
+        if(user) {
+            return console.log('User Exists')
+        } else  {
+            const user = new User();
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(req.body.password, salt);
+
+            user.name = req.body.name;
+            user.email = req.body.email;
+            user.password = hash;
+
+            user.save().then(user => {
+                // return res.status(200).json({ message: 'User Created', user });
+                return req.login(user, err => {
+                    if (err) {
+                        return res.status(500).json({message: 'Server error'});
+                    } else {
+                        res.redirect('/auth/options');
+                        // next();
+                    }
+                })
+            })
+            .catch(err => console.log(err));
+        }; 
+    });
+});
+// Logout user
+router.get('/logout', (req,res) => {
+  req.session.destroy();
+  console.log('logout...', req.session)
+  req.logout();
+  return res.redirect('/');
+}
+)
+
 
 module.exports = router;
