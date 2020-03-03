41d730da0443d2c51bae3cf2927994f1b935e269
jharte5 <jonathan.harte@codeimmersives.com>
2020-03-03 10:36:44 -0500

    few changes

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -15,6 +15,40 @@ router.get('/', function(req, res, next) {
   return res.render('index')
 });
 
+router.get('/api/users/login', (req, res) => {
+  return res.render('login')
+});
+
+router.get('/api/users/register', (req, res) => {
+  res.render('register')
+});
+
+router.get('/auth/options', (req, res) => {
+  if (req.isAuthenticated()){
+    return res.render('success')
+  }
+  else {
+    return res.render('fail')
+  }
+});
+
+router.get('/auth/movies', (req, res) => {
+  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}`;
+  const img = 'https://image.tmdb.org/t/p/w185';
+  if (req.isAuthenticated()) {
+    
+  fetch(url).then((res) => res.json()).then((movies) => {
+      const theMovies = movies.results
+      // console.log(theMovies)
+      res.render('movies', {theMovies, img})
+  })
+  .catch((err) => console.log(err))
+  }
+  else {
+    return res.render('fail')
+  }
+});
+
 router.get('/success', (req, res) => {
   if (req.isAuthenticated()){
     return res.render('success');
